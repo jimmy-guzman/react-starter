7a409bf30b022a747140a67bb8edf114105e41b1
jimmy-guzman <hi@jimmyguzman.com>
2023-04-02 18:37:18 -0500

    fix: 🐛 fix coverage & test scripts due to e2e conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
   test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
     coverage: { 100: true },
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    globals: true,
+    setupFiles: './src/test/setup.ts',
   },
 })
