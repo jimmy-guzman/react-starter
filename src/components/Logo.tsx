2eb54e18b35290f42a3ea020aad349e9c26c8e23
jimmy-guzman <hi@jimmyguzman.com>
2023-04-03 20:02:01 -0500

    fix: 🐛 apply dynamic drop shadow on logos

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,25 @@
 import clsx from 'clsx'
 
 interface LogoProps {
-  className?: string
-  color: string
   imgSrc: string
   link: string
-  name: string
+  name: 'React' | 'tailwindcss' | 'TypeScript' | 'Vite'
 }
 
-export const Logo = ({ color, className, link, name, imgSrc }: LogoProps) => {
+const classNames = {
+  Vite: 'hover:drop-shadow-[0_0_2em_#646CFF]',
+  React:
+    'hover:drop-shadow-[0_0_2em_#61DAFB] animate-[spin_20s_linear_infinite]',
+  TypeScript: 'hover:drop-shadow-[0_0_2em_#3178C6]',
+  tailwindcss: 'hover:drop-shadow-[0_0_2em_#06B6D4]',
+}
+
+export const Logo = ({ link, imgSrc, name }: LogoProps) => {
   return (
     <a href={link} target='_blank' rel='noreferrer'>
       <img
         src={imgSrc}
-        className={clsx(
-          `h-24 transition duration-300 hover:drop-shadow-[0_0_2em_${color}]`,
-          className
-        )}
+        className={clsx('h-24 transition duration-300', classNames[name])}
         alt={`${name} logo`}
       />
     </a>
