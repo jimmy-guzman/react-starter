e9d4e4fdc9d240f6375762e6c24e7d22868e1f62
jimmy-guzman <hi@jimmyguzman.com>
2023-04-25 19:31:13 -0500

    test: ✅ use `userEvent` v14 api instead of v13

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,11 +1,11 @@
 import App from './App'
-import { render, userEvent, screen } from './test/utils'
+import { render, screen } from './test/utils'
 
 describe('app', () => {
   it('should increase count', async () => {
-    render(<App />)
+    const { user } = render(<App />)
 
-    userEvent.click(screen.getByRole('button', { name: /count is 0/i }))
+    user.click(screen.getByRole('button', { name: /count is 0/i }))
 
     await expect(
       screen.findByRole('button', { name: /count is 1/i })
