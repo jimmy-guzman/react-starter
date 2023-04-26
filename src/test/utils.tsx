309a03c4b08638c30a969fd6c55f1fd4af21b1cf
jimmy-guzman <hi@jimmyguzman.com>
2023-04-25 19:31:13 -0500

    test: ✅ use `userEvent` v14 api instead of v13

---
+++ src/test/utils.tsx
@@ -0,0 +1,20 @@
+import { cleanup, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { afterEach } from 'vitest'
+
+afterEach(() => {
+  cleanup()
+})
+
+const customRender = (ui: React.ReactElement, options = {}) => {
+  return {
+    user: userEvent.setup(),
+    ...render(ui, {
+      wrapper: ({ children }) => children,
+      ...options,
+    }),
+  }
+}
+
+export * from '@testing-library/react'
+export { customRender as render }
