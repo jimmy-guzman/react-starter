348d8d8764be1eade4d174acc78bb9c4eea8492d
jimmy-guzman <hi@jimmyguzman.com>
2023-12-07 17:15:47 -0600

    feat: ✨ add `e2e` tests Github Actions worflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,36 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { configDefaults } from 'vitest/config'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  test: {
-    coverage: {
-      reporter: ['html', 'text-summary'],
-      exclude: [
-        '**/*.d.ts',
-        'storybook-static/**',
-        '.storybook/**',
-        '**/*.stories.*',
-        '**/main.tsx',
-        '{tailwind,postcss,playwright}.config.*',
-        '.{eslint,prettier}rc.{?(c|m)js,yml}',
-        'node_modules/.pnpm/**',
-      ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+    server: {
+      port: parseInt(env.PORT),
     },
-    environment: 'happy-dom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-  },
+    test: {
+      coverage: {
+        reporter: ['html', 'text-summary'],
+        exclude: [
+          '**/*.d.ts',
+          'storybook-static/**',
+          '.storybook/**',
+          '**/*.stories.*',
+          '**/main.tsx',
+          '{tailwind,postcss,playwright}.config.*',
+          '.{eslint,prettier}rc.{?(c|m)js,yml}',
+          'node_modules/.pnpm/**',
+        ],
+      },
+      environment: 'happy-dom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      globals: true,
+      setupFiles: './src/test/setup.ts',
+    },
+  }
 })
