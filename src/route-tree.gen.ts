0c47ff2f84d51c56ca8a6c9e5d1a481ec5a3d48e
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-05-15 05:21:11 -0500

    feat: ✨ enable expiremental routing code splitting (#94)

--- src/route-tree.gen.ts
+++ src/route-tree.gen.ts
@@ -25,6 +25,9 @@ const IndexRoute = IndexImport.update({
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
       preLoaderRoute: typeof IndexImport;
       parentRoute: typeof rootRoute;
     };
@@ -33,6 +36,6 @@ declare module "@tanstack/react-router" {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([IndexRoute]);
+export const routeTree = rootRoute.addChildren({ IndexRoute });
 
 /* prettier-ignore-end */
