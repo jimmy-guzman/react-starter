af83b7cd7f7489ccd3fc778874ed2f50e65e9225
jimmy-guzman <hi@jimmyguzman.com>
2024-07-14 13:56:03 -0500

    ci: 👷 bump `storybook` to v8.2.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,9 +13,7 @@ const config = {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 } satisfies StorybookConfig;
 
 export default config;
