725777fa5f2694aed5e568cbbbf263eff284f217
jimmy-guzman <hi@jimmyguzman.com>
2025-03-31 18:15:40 -0500

    refactor: ♻️ expand `<Button />` props

--- src/components/ui/button.stories.tsx
+++ src/components/ui/button.stories.tsx
@@ -15,30 +15,82 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    children: "daisyUI",
+export const Colors: Story = {
+  render: (args) => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        {(
+          [
+            "primary",
+            "secondary",
+            "accent",
+            "info",
+            "success",
+            "warning",
+            "error",
+            "neutral",
+            "default",
+          ] as const
+        ).map((color) => {
+          return (
+            <Button key={color} {...args} color={color}>
+              {color}
+            </Button>
+          );
+        })}
+      </div>
+    );
   },
 };
 
-export const Primary: Story = {
-  args: {
-    children: "Primary",
-    color: "primary",
+export const Sizes: Story = {
+  render: (args) => {
+    return (
+      <div className="flex flex-wrap items-end gap-2">
+        {(["xs", "sm", "default", "lg", "xl"] as const).map((size) => {
+          return (
+            <Button key={size} {...args} size={size}>
+              {size}
+            </Button>
+          );
+        })}
+      </div>
+    );
   },
 };
 
-export const Outline: Story = {
-  args: {
-    children: "Outline",
-    variant: "outline",
+export const Variants: Story = {
+  render: (args) => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        {(["default", "ghost", "link", "outline", "soft"] as const).map(
+          (variant) => {
+            return (
+              <Button key={variant} {...args} variant={variant}>
+                {variant}
+              </Button>
+            );
+          },
+        )}
+      </div>
+    );
   },
 };
 
-export const Link: Story = {
-  args: {
-    asChild: true,
-    children: <a href="#link">Link</a>,
-    variant: "link",
+export const Modifiers: Story = {
+  render: (args) => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        {(["default", "block", "wide", "circle", "square"] as const).map(
+          (modifier) => {
+            return (
+              <Button key={modifier} {...args} modifier={modifier}>
+                {modifier}
+              </Button>
+            );
+          },
+        )}
+      </div>
+    );
   },
 };
